987c064b96d5ea047c699252b70b25cdffe49eb1
samandarwebdeveloper <86816538+samandarwebdeveloper@users.noreply.github.com>
2021-09-07 22:49:13 +0500

    Add JS codes

---
+++ js/index.js
@@ -0,0 +1,15 @@
+var intro = alert("Xush kelibsiz bizning saytga!");
+var intro = alert("Bizning jamoada ishlash uchun iltimos ma\'lumotlarni to\'ldiring!");
+var firstName = prompt("Ismingizni kiriting", "John").trim();
+var sureName = prompt("Sharifingizni kiriting!", "Doe").trim();
+var age = prompt("Yoshingizni kiriting!", "Age").trim();
+var job = prompt("Jamoamizda qaysi kasbda ishlamoqchisiz?", "Job").trim();
+var number = prompt("Telefon raqamingiz", "Number").trim();
+var intro = alert("Raxmat, Siz bilan tez orada a\'loqaga chiqamiz!");
+
+console.log(`Ismingiz: ${firstName}\n Sharifingiz: ${sureName}\n Yoshingiz: ${age}\n Kasbingiz: ${job}\n Telefon raqamingiz: ${number}`);
+
+
+
+
+
